942137aa756094225c5c5fbf9480fe578c05d612
nishant666 <nishants440@gmail.com>
2020-08-08 21:11:47 +0530

    Updates

---
+++ precache-manifest.2b7a5dd6a135c327334bd437a2513ca3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4040af494d62a902a826925abea4de8e",
+    "url": "/Hangman-react/index.html"
+  },
+  {
+    "revision": "06d60996e1f04d68fe4b",
+    "url": "/Hangman-react/static/css/main.1e16d5d3.chunk.css"
+  },
+  {
+    "revision": "518c31b860db7e505983",
+    "url": "/Hangman-react/static/js/2.56593433.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Hangman-react/static/js/2.56593433.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06d60996e1f04d68fe4b",
+    "url": "/Hangman-react/static/js/main.3de74836.chunk.js"
+  },
+  {
+    "revision": "ab9f36969db4a2da575f",
+    "url": "/Hangman-react/static/js/runtime-main.08393c0d.js"
+  }
+]);
